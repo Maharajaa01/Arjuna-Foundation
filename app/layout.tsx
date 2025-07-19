6d8a86cbcd7e734871dda898710f16bc1c078433
Maharaja <114376134+Maharajaa01@users.noreply.github.com>
2025-07-20 00:45:39 +0530

    Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Arjuna Foundation",
+  title: "Arjuna Fooundation",
 };
 
 export default function RootLayout({
